ac493d47e790ba8e3b13279b7275e6a4a26ba593
Adrian <adrian@globalrose.com>
2015-09-04 15:31:16 -0400

    pset 7.js

--- psets/7.js
+++ psets/7.js
@@ -11,6 +11,23 @@ For example:
 fixStart('babble'): 'ba**le'
 */
 
+//still not working!!!!----!!!--!-!_!_!_
+
 function fixStart(s) {
-  // write your solution here...
+  /*var char = s.charAt(0);
+  var s2 = char;
+  for (var i = 1; i < s.length; i++) {
+    if (s[i] == char) {
+      s2=s2+ '*';
+    }
+    else {
+      s2 = s2+ s[i];
+    }
+  }
+  return s2;*/
+  return s[0]+s.substring(1).replace(RegExp(s[0],'g'),"*");
 }
+
+var word = "babble";
+var fix = fixStart(word);
+console.log(fix);
